8a4f31aa6a26203f09516f2bad19002a16013766
Andreas Grabner <grabnerandi@gmx.at>
2018-09-04 16:31:46 +0200

    added new params

--- launch_test.sh
+++ launch_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Starts a JMeter test that is passed via command line arguments
 # Usage:
-# ./run_test.sh testscript.jmx result.jtl yourserverip [MyLoadTestName]
+# ./run_test.sh testscript.jmx result.jtl yourserverip [yourpath] [VUCount] [LoopCount] [MyLoadTestName]
 
 if [ -z "$1" ]; then
   echo "Usage: Arg 1 needs to be valid <yourtestscript>.jmx"
@@ -15,11 +15,15 @@ if [ -z "$3" ]; then
   echo "Usage: Arg 3 needs to be the URL or IP of your service that should be tested"
   exit 1
 fi
-DT_LTN=$4
+CHECK_PATH=$4
 if [ -z "$4" ]; then
+  CHECK_PATH=/
+fi
+DT_LTN=$5
+if [ -z "$5" ]; then
   DT_LTN=MyLoadTestName
 fi
 
 sudo rm -f -r $2
 sudo mkdir $2
-sudo docker run --name jmeter-test -v "${PWD}/scripts":/scripts -v "${PWD}/$2":/results --rm -d jmeter ./jmeter/bin/jmeter.sh -n -t /scripts/$1 -e -o /results -l result.tlf -JSERVER_UR="$3" -JDT_LTN="$DT_LTN"
+sudo docker run --name jmeter-test -v "${PWD}/scripts":/scripts -v "${PWD}/$2":/results --rm -d jmeter ./jmeter/bin/jmeter.sh -n -t /scripts/$1 -e -o /results -l result.tlf -JSERVER_URL="$3" -JDT_LTN="$DT_LTN" -JCHECK_PATH="$CHECK_PATH"
